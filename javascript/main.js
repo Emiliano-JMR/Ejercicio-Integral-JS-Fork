739a6e918704eb4d2afd527752a1ba46562d0e0a
EmilianoJMR <emiliano.mendozar.dev@gmail.com>
2026-05-29 10:13:39 -0600

    Añadiendo funcionalidades

---
+++ javascript/main.js
@@ -0,0 +1,15 @@
+import { navBar } from "./navbar.js";
+import { countLikes } from "./likes.js";
+import { showDetails } from "./prodDescription.js";
+import { changeAvailability } from "./availability.js";
+
+
+document.addEventListener("DOMContentLoaded",() =>{
+    navBar();
+    countLikes();
+    showDetails();
+    changeAvailability();
+}) ;
+
+  
+//se manda a llamar las funciones 
